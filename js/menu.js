f10e7d84167b6654102524dcc1e76107ab16b706
oCrazyoff <walyssonribeiro3@gmail.com>
2025-04-16 13:39:27 -0300

    add: implement avanços container and integrate with menu functionality

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,16 @@
 const containerPoderes = document.querySelector('.container-poderes');
 const containerUpgrades = document.querySelector('.container-upgrades');
+const containerAvancos = document.querySelector('.container-avancos');
 const containerConfig = document.querySelector('.container-config');
 const menuPoderes = document.getElementById('menu-poderes');
 const menuUpgrades = document.getElementById('menu-upgrades');
+const menuAvancos = document.getElementById('menu-avancos');
 const menuConfig = document.getElementById('menu-config');
+
 const menus = [
     { button: menuPoderes, container: containerPoderes },
     { button: menuUpgrades, container: containerUpgrades },
+    { button: menuAvancos, container: containerAvancos },
     { button: menuConfig, container: containerConfig },
 ];
 
@@ -23,4 +27,5 @@ function handleMenuClick(activeMenu) {
 
 menuPoderes.addEventListener('click', () => handleMenuClick(menus[0]));
 menuUpgrades.addEventListener('click', () => handleMenuClick(menus[1]));
-menuConfig.addEventListener('click', () => handleMenuClick(menus[2]));
+menuAvancos.addEventListener('click', () => handleMenuClick(menus[2]));
+menuConfig.addEventListener('click', () => handleMenuClick(menus[3]));
